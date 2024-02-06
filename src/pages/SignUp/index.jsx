0d6f4b73169889d19d6d98098b609f9591d8e69b
erisonn <erison.frontend@gmail.com>
2024-02-06 18:13:47 -0300

    Add account creation form component

--- src/pages/SignUp/index.jsx
+++ src/pages/SignUp/index.jsx
@@ -1,4 +1,43 @@
+import { useEffect, useState } from "react";
+import { CreateLocalStorageAccount } from "../../helpers/createLocalStorageAccount";
+import { useNavigate } from "react-router-dom";
+
 const SignUp = () => {
-  return <>SGIN UP</>;
+  const [isAccountCreateSuccessful, setIsAccountCreateSuccessful] =
+    useState(null);
+
+    const navigate = useNavigate()
+
+  const handleSubmit = (event) => {
+    const user = event.target[0].value;
+    const password = event.target[1].value;
+
+    event.preventDefault();
+
+    CreateLocalStorageAccount(user, password, setIsAccountCreateSuccessful);
+  };
+
+  useEffect(() => {
+    if (isAccountCreateSuccessful) {
+      setTimeout(() => {
+        navigate('login')
+      }, 2000);
+    }
+  }, [isAccountCreateSuccessful]);
+
+  if (isAccountCreateSuccessful) {
+    return <h2>Account created sucessfully!</h2>;
+  }
+
+  return (
+    <div>
+      <form onSubmit={handleSubmit}>
+        <input type="text" />
+        <input type="password" />
+        <input type="submit" value={"Create account"} />
+        {isAccountCreateSuccessful === false && <p>Username not available</p>}
+      </form>
+    </div>
+  );
 };
 export default SignUp;
