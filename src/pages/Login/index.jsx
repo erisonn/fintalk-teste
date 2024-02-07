45278cf403f5fff488d903db098936d012448421
erisonn <erison.frontend@gmail.com>
2024-02-06 21:24:57 -0300

    add logged user redirect to login page

--- src/pages/Login/index.jsx
+++ src/pages/Login/index.jsx
@@ -1,7 +1,15 @@
+import { Navigate } from "react-router-dom";
 import LoginForm from "../../components/LoginForm";
+import { getLoggedUser } from "../../helpers/getLoggedUser";
 import "./index.scss";
 
 const Login = () => {
+  const loggedUser = getLoggedUser();
+
+  if (loggedUser?.user) {
+    return <Navigate to="/messages" />;
+  }
+
   return (
     <div className="Login-page">
       <div className="Left-container">
