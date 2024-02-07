12e5e901db65e5e2c311bf7400e2d869d1b7695d
erisonn <erison.frontend@gmail.com>
2024-02-07 17:04:29 -0300

    add chat content from mock data

--- src/components/ChatContent/index.jsx
+++ src/components/ChatContent/index.jsx
@@ -1,6 +1,13 @@
-import './index.scss'
+import "./index.scss";
+import { chatMock } from "../../mock";
+import dayjs from "dayjs";
+import relativeTime from "dayjs/plugin/relativeTime";
+import { Navigate } from "react-router-dom";
 
+dayjs.extend(relativeTime);
 const ChatContent = ({ id }) => {
+  const currentChat = chatMock.find((chat) => String(chat.id) === String(id));
+
   if (!id) {
     return (
       <div className="ChatContent">
@@ -9,7 +16,23 @@ const ChatContent = ({ id }) => {
     );
   }
 
-  return <p>Chat selected</p>;
+  if (!currentChat) {
+    return <Navigate to="/404" />;
+  }
+
+  return (
+    <div className="ChatContent">
+      <h1>{currentChat.title}</h1>
+      <div className="ChatContent-messages">
+        {currentChat.messages.map((message) => (
+          <p key={message.date}>
+            {dayjs.unix(message.date).format("hh:mm")} <b>{message.user}</b>:{" "}
+            {message.message}
+          </p>
+        ))}
+      </div>
+    </div>
+  );
 };
 
 export default ChatContent;
