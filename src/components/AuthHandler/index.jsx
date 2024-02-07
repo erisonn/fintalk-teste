ab232f46d7971ae9962a9958ad16dcf377670456
erisonn <erison.frontend@gmail.com>
2024-02-06 21:04:24 -0300

    check if user is logged on AuthHandler component

--- src/components/AuthHandler/index.jsx
+++ src/components/AuthHandler/index.jsx
@@ -1,11 +1,12 @@
 import { Navigate, useLocation } from "react-router-dom";
+import { getLoggedUser } from "../../helpers/getLoggedUser";
 
 const AuthHandler = ({ children }) => {
+  const loggedUser = getLoggedUser();
   const location = useLocation();
-  const isLoggedIn = false;
 
   const invalidPathnames = ["/login", "/sign-up"];
-  if (!isLoggedIn && !invalidPathnames.includes(location.pathname)) {
+  if (!loggedUser?.user && !invalidPathnames.includes(location.pathname)) {
     return <Navigate to="/login" />;
   }
 
