d1ca97e11821f4a9dad211f65f4539bc7f1386b7
erisonn <erison.frontend@gmail.com>
2024-02-07 17:03:30 -0300

    add onclick to chat list item

--- src/components/ChatList/index.jsx
+++ src/components/ChatList/index.jsx
@@ -1,8 +1,14 @@
-import { chatMock } from "../ChatList/mock";
+import { chatMock } from "../../mock";
 import "./index.scss";
 import ChatLogo from "../../assets/chat-icon.svg?react";
+import { useNavigate } from "react-router-dom";
 
 const ChatList = () => {
+  const navigate = useNavigate();
+  const handleClick = (id) => {
+    navigate(`/messages/${id}`);
+  };
+
   return (
     <div className="ChatList">
       <div className="ChatList-header">
@@ -13,13 +19,19 @@ const ChatList = () => {
         {chatMock.map((chat) => {
           const lastMessage = chat.messages[chat.messages.length - 1];
           return (
-            <div className="ChatList-item" key={chat.id}>
+            <div
+              className="ChatList-item"
+              key={chat.id}
+              onClick={() => handleClick(chat.id)}
+            >
               <div className="ChatList-item-logo">
                 <ChatLogo />
               </div>
               <div className="ChatList-item-content">
                 <h4>{chat.title}</h4>
-                <p>{lastMessage.user}: {lastMessage.message}</p>
+                <p>
+                  {lastMessage.user}: {lastMessage.message}
+                </p>
               </div>
             </div>
           );
