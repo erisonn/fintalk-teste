9242c73fd6cad0758f8328a26026285bd5356dda
erisonn <erison.frontend@gmail.com>
2024-02-08 15:21:55 -0300

    implement generic button

--- src/components/Form/index.jsx
+++ src/components/Form/index.jsx
@@ -1,5 +1,6 @@
 import "./index.scss";
 import { Link } from "react-router-dom";
+import Button from "../Button";
 
 const Form = ({ handleSubmit, isSignUp, buttonText, warningText }) => {
   return (
@@ -7,14 +8,24 @@ const Form = ({ handleSubmit, isSignUp, buttonText, warningText }) => {
       <form onSubmit={handleSubmit} className="Form-content">
         <div>
           <h3>Username</h3>
-          <input type="text" name="username" required pattern="^[0-9A-Za-z]{6,16}$" />
+          <input
+            type="text"
+            name="username"
+            required
+            pattern="^[0-9A-Za-z]{6,16}$"
+          />
           <p className="Form-reasoning">
             Must be between 6 and 16 alphanumeric characters
           </p>
         </div>
         <div>
           <h3>Password</h3>
-          <input type="password" name="password" required pattern="^[0-9A-Za-z]{8,32}$" />
+          <input
+            type="password"
+            name="password"
+            required
+            pattern="^[0-9A-Za-z]{8,32}$"
+          />
           <p className="Form-reasoning">
             Must be between 8 and 32 alphanumeric characters
           </p>
@@ -35,10 +46,11 @@ const Form = ({ handleSubmit, isSignUp, buttonText, warningText }) => {
               </Link>
             </p>
           )}
-          <input
-            className="Form-content-button"
-            type="submit"
-            value={buttonText}
+
+          <Button
+            buttonText={buttonText}
+            buttonHeight={35}
+            buttonWidth={"100%"}
           />
           {warningText && <p className="Warning-text">{warningText}</p>}
         </div>
